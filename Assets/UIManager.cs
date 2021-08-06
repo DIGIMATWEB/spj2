a5f69e5442577cae5525d040acdd42bc262ede01
Heladio <inghfrancisco.morales@gmail.com>
2021-08-05 23:42:30 -0500

    script register validation not null

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public RectTransform inicioScreen, regiterScreen, emailScreen, bannerScreen;// mainMenu, carShopMenu, powerShopMenu;
+   
+    [Header("Fields Login")]
+    public TMP_InputField Usr;
+    public TMP_InputField Pass;
+
+
+
+    [Header("Fields Registro")]
+    public TMP_InputField nombre;// campo de texto para introduci datos de nombre 
+    public TMP_InputField apellido;
+    public TMP_InputField pais;// campo de texto para introduci datos de nombre 
+    public TMP_InputField email;
+    public TMP_InputField contraseña;
+    public Toggle terms;
+
     void Start()
     {
         inicioScreen.DOAnchorPos(Vector2.zero, 0.2f);
@@ -30,12 +46,25 @@ public class UIManager : MonoBehaviour
 
     public void sendRegisterButton()
     {
-        regiterScreen.DOAnchorPos(new Vector2(-1400, 0), 0.25f);
-        emailScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+       // logValidation();
+      
+
+       if( nombre.text != "")
+        {
+            Debug.Log("Fields void   " + nombre.text + " | " + apellido.text + " | " + pais.text + " | " + email.text + " | " + contraseña.text + " | " + terms.isOn);
+        }
+        // regiterScreen.DOAnchorPos(new Vector2(-1400, 0), 0.25f);
+        // emailScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
     }
     public void bannerScreenButton()
     {
         emailScreen.DOAnchorPos(new Vector2(-1400, 0), 0.25f);
         bannerScreen.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+
+    public void logValidation()
+    {
+        Debug.Log("Fields void   " + nombre.text + " | " + apellido.text + " | " + pais.text + " | " + email.text + " | " + contraseña.text + " | " + terms.isOn);
     }
 }
