036085a0f43b4c4bc50fc235a90c9791c7912d2a
PabloLaMontOrozco <lamont.pablo@gmail.com>
2021-08-10 11:25:38 -0500

    Fixed Inputfields, fixed buton for video, readded lost scripts To do: - fix UIManager references in the buttons - fix reddirect buttons for URL links

---
+++ Assets/spacejam2/Instrucciones.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Instrucciones : MonoBehaviour
+{
+    public Toggle onoff;
+
+    public GameObject instrucciones;
+    public GameObject video;
+
+    private bool isOn;
+
+    public void TurnOnOffInstr() 
+    {
+        isOn = onoff.isOn;
+
+        if (isOn) 
+        {
+            instrucciones.SetActive(true);
+            video.GetComponentInChildren<VideoPlayer>().Stop();
+            video.SetActive(false);
+            isOn = false;
+        }
+        else 
+        {
+            instrucciones.SetActive(false);
+            video.SetActive(true);
+            video.GetComponentInChildren<VideoPlayer>().Play();
+            isOn = true;
+        }
+    }
+}
